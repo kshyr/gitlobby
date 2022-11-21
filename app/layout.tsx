ddc8c9896130530c39ab9f1a784a8d762d470f17
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-11-21 03:22:03 -0500

    grid layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import './globals.css'
+import "../styles/globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +12,17 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="h-screen grid grid-cols-1 grid-rows-[104px_1fr_55px] border [&>*]:border-[0.5px] [&>*]:border-white border-white border-opacity-20 [&>*]:border-opacity-10 ">
+          <header className="p-2">
+            <h1 className="text-8xl font-bold">GitHub Hub</h1>
+          </header>
+          {children}
+          <footer className="p-2">
+            <p className="text-4xl font-semibold">Footer</p>
+          </footer>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
