7a8443f2d9e20ce52d39c2746b957e516e9cc322
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-11-21 03:36:54 -0500

    tailwind class refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       */}
       <head />
       <body>
-        <div className="h-screen grid grid-cols-1 grid-rows-[104px_1fr_55px] border [&>*]:border-[0.5px] [&>*]:border-white border-white border-opacity-20 [&>*]:border-opacity-10 ">
+        <div className="grid-layout">
           <header className="p-2">
             <h1 className="text-8xl font-bold">GitHub Hub</h1>
           </header>
