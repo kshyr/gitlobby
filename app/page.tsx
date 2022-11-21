7f18f2a5c60bd28dda5113bb654b21a95da87853
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-11-21 12:33:13 -0500

    trying out grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
     <div className="home">
-      <h1 className="text-6xl text-justify">Home Page</h1>
+      <main className="grid grid-rows-[500px_105px] [&>*]:p-2">
+        <div className="grid grid-cols-[650px_1fr_1fr] [&>*]:text-center">
+          <h2 className="text-5xl border-r border-white border-opacity-20">
+            Popular repositories
+          </h2>
+          <h2 className="text-5xl border-r border-white border-opacity-20">
+            Users
+          </h2>
+          <h2 className="text-5xl">Organizations</h2>
+        </div>
+
+        <h1 className="border-t border-white border-opacity-20">smth</h1>
+      </main>
     </div>
   );
 }
