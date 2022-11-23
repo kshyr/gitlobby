41ca886e21345bf547d08810309801af48de0972
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-11-23 00:39:39 -0500

    animation on page load

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,47 @@
 export default async function Home() {
   return (
-    <div className="home h-full">
-      <main className="grid h-full grid-rows-1 md:grid-rows-[400px_1fr]">
-        <div className="grid grid-cols-1 md:grid-cols-[650px_1fr_1fr]">
-          <div className="home-block group/repos border-r hover:origin-top-left">
-            <h2 className="text-5xl group-hover/repos:text-black">
-              Popular repositories
-            </h2>
-            <p className="text-center text-xl group-hover/repos:text-black">
-              Find the most popular repositories on GitHub
-            </p>
-          </div>
-          <div className="group/users home-block border-r hover:origin-top">
-            <h2 className="text-5xl group-hover/users:text-black">Users</h2>
-            <p className="text-center text-xl group-hover/users:text-black">
-              Find out more about users
-            </p>
-          </div>
-          <div className="group/orgs home-block hover:origin-top-right">
-            <h2 className="text-5xl group-hover/orgs:text-black">
-              Organizations
-            </h2>
-            <p className="text-center text-xl group-hover/orgs:text-black">
-              Find organizations on GitHub
-            </p>
-          </div>
+    <main className="grid h-full grid-rows-1 md:grid-rows-[400px_1fr]">
+      <div className="grid grid-cols-1 md:grid-cols-[650px_1fr_1fr]">
+        <div className="home-block group/repos animate-slide-in-1 border-r hover:origin-top-left">
+          <h2 className="text-5xl group-hover/repos:text-black">
+            Popular repositories
+          </h2>
+          <p className="text-center text-xl group-hover/repos:text-black">
+            Find the most popular repositories on GitHub
+          </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-[650px_1fr]">
-          <div className="home-block group/profile border-r border-t hover:origin-bottom-left">
-            <h2 className="text-5xl group-hover/profile:text-black">
-              Your profile
-            </h2>
-            <p className="text-center text-xl group-hover/profile:text-black">
-              Track your activity on GitHub
-            </p>
-          </div>
-          <div className="group/activity home-block border-t hover:origin-bottom-right">
-            <h2 className="text-5xl group-hover/activity:text-black">Plans</h2>
-            <p className="text-center text-xl group-hover/activity:text-black">
-              Store your TODOs and future ideas
-            </p>
-          </div>
+        <div className="group/users home-block animate-slide-in-2 border-r hover:origin-top">
+          <h2 className="text-5xl group-hover/users:text-black">Users</h2>
+          <p className="text-center text-xl group-hover/users:text-black">
+            Find out more about users
+          </p>
         </div>
-      </main>
-    </div>
+        <div className="group/orgs home-block animate-slide-in-3 hover:origin-top-right">
+          <h2 className="text-5xl group-hover/orgs:text-black">
+            Organizations
+          </h2>
+          <p className="text-center text-xl group-hover/orgs:text-black">
+            Find organizations on GitHub
+          </p>
+        </div>
+      </div>
+      <div className="grid animate-slide-in-4 grid-cols-1 md:grid-cols-[650px_1fr]">
+        <div className="home-block group/profile border-r border-t hover:origin-bottom-left">
+          <h2 className="text-5xl group-hover/profile:text-black">
+            Your profile
+          </h2>
+          <p className="text-center text-xl group-hover/profile:text-black">
+            Track your activity on GitHub
+          </p>
+        </div>
+        <div className="group/activity home-block animate-slide-in-5 border-t hover:origin-bottom-right">
+          <h2 className="text-5xl group-hover/activity:text-black">Plans</h2>
+          <p className="text-center text-xl group-hover/activity:text-black">
+            Store your TODOs and future ideas
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'slide-in-1': 'slide-in-left 1.4s ease-out',
+        'slide-in-2': 'slide-in-left 1.3s ease-out',
+        'slide-in-3': 'slide-in-left 0.9s ease-out',
+        'slide-in-4': 'slide-in-right 1.5s ease-out',
+        'slide-in-5': 'slide-in-right 1.8s ease-out',      
+      },
+      keyframes: {
+        'slide-in-left': {
+          "0%": { transform: "translateX(-400%)", borderColor: "#ffffff00", opacity: 0 },
+          "80%": { transform: "translateX(0)", borderColor: "#ffffff00", opacity: 1 },
+          "100%": { transform: "translateX(0)", borderColor: "#ffffff33", opacity: 1 },
+        },
+        'slide-in-right': {
+          "0%": { transform: "translateX(200%)", borderColor: "#ffffff00", opacity: 0 },
+          "80%": { transform: "translateX(0)", borderColor: "#ffffff00", opacity: 0 },
+          "100%": { transform: "translateX(0)", borderColor: "#ffffff33", opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 }
