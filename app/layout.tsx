38d5c014edd81c748916a709d5f08941cb0b44aa
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-11-22 21:06:15 -0500

    add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
-
+import GitHubLogo from "../public/github.svg";
+import Image from "next/image";
 export default function RootLayout({
   children,
 }: {
@@ -14,8 +15,15 @@ export default function RootLayout({
       <head />
       <body>
         <div className="grid-layout">
-          <header className="p-2">
-            <h1 className="text-8xl font-bold">GitHub Hub</h1>
+          <header className="flex justify-start justify-items-center gap-2 p-2 px-6">
+            <Image
+              src="/github.svg"
+              alt="GitHub Logo"
+              className="invert"
+              width={64}
+              height={64}
+            />
+            <h1 className="text-6xl font-bold">GitLobby</h1>
           </header>
           {children}
           <footer className="p-2">
