60329e1fb32466d316dab6c1bea9fbdbd75550b4
Kostiantyn Shyrolapov <ks1379@scarletmail.rutgers.edu>
2022-12-27 01:22:39 -0500

    fix: :lipstick: repsonsive design part 1
    make grid widths responsive, move home block children css to tailwind classes

--- app/page.tsx
+++ app/page.tsx
@@ -2,48 +2,52 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <main className="grid h-full grid-rows-1 overflow-hidden md:grid-rows-[400px_1fr]">
-      <div className="grid grid-cols-1 md:grid-cols-[650px_1fr_1fr]">
+      <div className="grid grid-flow-row grid-cols-1 md:grid-cols-[2fr_1fr_1fr]">
         <Link href="/repos">
-          <div className="home-block group/repos animate-slide-in-1 border-r hover:origin-top-left">
-            <h2 className="text-5xl group-hover/repos:text-black">
+          <div className="home-block group/repos flex animate-slide-in-1 items-center justify-center border-r hover:origin-top-left">
+            <h2 className="home-block-title group-hover/repos:text-black">
               Popular repositories
             </h2>
-            <p className="text-center text-xl group-hover/repos:text-black">
+            <p className="home-block-description group-hover/repos:text-black">
               Find the most popular repositories on GitHub
             </p>
           </div>
         </Link>
         <div className="group/users home-block animate-slide-in-2 border-r hover:origin-top">
           <img src="undermaintenance.png" className="undermaintenance" />
-          <h2 className="text-5xl group-hover/users:text-black">Users</h2>
-          <p className="text-center text-xl group-hover/users:text-black">
+          <h2 className="home-block-title group-hover/users:text-black">
+            Users
+          </h2>
+          <p className="home-block-description group-hover/users:text-black">
             Find out more about users
           </p>
         </div>
         <div className="group/orgs home-block animate-slide-in-3 hover:origin-top-right">
           <img src="undermaintenance.png" className="undermaintenance" />
-          <h2 className="text-5xl group-hover/orgs:text-black">
-            Organizations
+          <h2 className="home-block-title group-hover/orgs:text-black">
+            Issues
           </h2>
-          <p className="text-center text-xl group-hover/orgs:text-black">
-            Find organizations on GitHub
+          <p className="home-block-description group-hover/orgs:text-black">
+            Find issues to start contributing
           </p>
         </div>
       </div>
-      <div className="grid animate-slide-in-4 grid-cols-1 md:grid-cols-[650px_1fr]">
+      <div className="grid animate-slide-in-4 grid-flow-row grid-cols-1 md:grid-cols-[1fr_1fr]">
         <div className="home-block group/profile border-r border-t hover:origin-bottom-left">
           <img src="undermaintenance.png" className="undermaintenance" />
-          <h2 className="text-5xl group-hover/profile:text-black">
+          <h2 className="home-block-title group-hover/profile:text-black">
             Your profile
           </h2>
-          <p className="text-center text-xl group-hover/profile:text-black">
+          <p className="home-block-description group-hover/profile:text-black">
             Track your activity on GitHub
           </p>
         </div>
         <div className="group/activity home-block animate-slide-in-5 border-t hover:origin-bottom-right">
           <img src="undermaintenance.png" className="undermaintenance" />
-          <h2 className="text-5xl group-hover/activity:text-black">Plans</h2>
-          <p className="text-center text-xl group-hover/activity:text-black">
+          <h2 className="home-block-title group-hover/activity:text-black">
+            Plans
+          </h2>
+          <p className="home-block-description group-hover/activity:text-black">
             Store your TODOs and future ideas
           </p>
         </div>
